b4523fd1117dcff5d728f205b8d5527c24442965
TaigaTi <tarikabirch@gmail.com>
2025-09-26 19:21:11 -0400

    Added mouse hover effect

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo.png"
 function Navbar() {
     return (
         <>
-            <div id="nav-bar" className="flex justify-between p-5 px-8 absolute w-full left-0">
+            <div id="nav-bar" className="flex justify-between p-5 px-8 absolute w-full left-0 z-50">
                 <div id="branding">
                     <img id="logo" src={logo} width={200}></img>
                     
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,26 @@
+import { useRef } from "react";
 import "./Home.css"
 
 function Home() {
+    const shadowRef = useRef<HTMLDivElement>(null);
+
+    const handleMouseMove = (e: React.MouseEvent) => {
+        if (!shadowRef.current) return;
+
+        const rect = (e.currentTarget as HTMLDivElement).getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        shadowRef.current.style.transform = `translate(${x - 300}px, ${y - 300}px)`;
+    };
+
     return (
-        <div id="home-landing" className="flex flex-col justify-center items-center h-[100vh]">
+        <div id="home-landing" className="flex flex-col justify-center items-center h-[100vh]"
+            onMouseMove={handleMouseMove}>
             <h1 className="text-2xl lg:text-3xl xl:text-4xl p-4 font-semibold">Building Your Vision, One Piece at a Time</h1>
             <p className="text-md lg:text-lg xl:text-xl text-[var(--light-grey)]">Software Design & Development Services</p>
             <button id="build-button" className="mt-6">Let's Build!</button>
+            <div ref={shadowRef} className="mouse-shadow"></div>
         </div>
     )
 
