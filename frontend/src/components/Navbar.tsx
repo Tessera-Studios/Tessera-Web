bf93b1db7803b09e230793756ef1bf4c1488ec75
TaigaTi <tarikabirch@gmail.com>
2025-09-29 19:26:24 -0400

    Added hover and active effects to nav links

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,4 +1,6 @@
+import "./Navbar.css"
 import logo from "../assets/logo.png"
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
     return (
@@ -6,14 +8,26 @@ function Navbar() {
             <div id="nav-bar" className="flex justify-between p-5 px-8 absolute w-full left-0 z-50">
                 <div id="branding">
                     <img id="logo" src={logo} width={200}></img>
-                    
+
                 </div>
 
-                <div id="links" className="flex md:gap-8 md:px-5 ">
-                    <a href="/" className="px-2">Home</a>
+                <div id="links" className="flex md:gap-12 md:px-5 mr-2">
+                    <NavLink to="/" className={({ isActive }) => isActive ? 'active' : '' + 'nav-link'}>
+                        Home
+                    </NavLink>
+                    <NavLink to="/about" className={({ isActive }) => isActive ? 'active' : '' + 'nav-link'}>
+                        About
+                    </NavLink>
+                    <NavLink to="/services" className={({ isActive }) => isActive ? 'active' : '' + 'nav-link'}>
+                        Services
+                    </NavLink>
+                    <NavLink to="/contact" className={({ isActive }) => isActive ? 'active' : '' + 'nav-link'}>
+                        Contact
+                    </NavLink>
+                    {/* <a href="/" className="px-2">Home</a>
                     <a href="/about" className="px-2">About</a>
                     <a href="/services" className="px-2">Services</a>
-                    <a href="/contact" className="px-2">Contact</a>
+                    <a href="/contact" className="px-2">Contact</a> */}
                 </div>
             </div>
         </>
