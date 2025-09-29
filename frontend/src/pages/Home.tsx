224129e6b77d0648e62c31d696a9dadf1084ca42
TaigaTi <tarikabirch@gmail.com>
2025-09-29 19:51:21 -0400

    Removed mouse shadow

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,27 +6,13 @@ import tesseraTileImage from "../assets/tessera-emblem-light-purple-transparent.
 import tesseraWhiteTileImage from "../assets/tessera-emblem-white-transparent.png"
 
 function Home() {
-    const shadowRef = useRef<HTMLDivElement>(null);
-
-    const handleMouseMove = (e: React.MouseEvent) => {
-        if (!shadowRef.current) return;
-
-        const rect = (e.currentTarget as HTMLDivElement).getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-
-        shadowRef.current.style.transform = `translate(${x - 300}px, ${y - 300}px)`;
-    };
-
     return (
         <>
-            <div id="home-landing" className="flex flex-col justify-center items-center min-h-[100vh]"
-                onMouseMove={handleMouseMove}>
+            <div id="home-landing" className="flex flex-col justify-center items-center min-h-[100vh]">
                 <div>
                     <h1 className="text-2xl lg:text-3xl xl:text-4xl p-4 font-semibold">Building Your Vision, One Piece at a Time</h1>
                     <p className="text-md lg:text-lg xl:text-xl text-[var(--light-grey)]">Software Design & Development Services</p>
                     <button id="build-button" className="mt-6">Let's Build!</button>
-                    <div ref={shadowRef} className="mouse-shadow"></div>
                 </div>
             </div>
 
