e6b03af8f497a0aa041c8d20a6d8bd7cd8855f57
TaigaTi <tarikabirch@gmail.com>
2025-09-30 17:48:00 -0400

    Added mobile responsiveness and navbar menu

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,37 +1,127 @@
-import "./Navbar.css"
-import logo from "../assets/logo.png"
+import "./Navbar.css";
+import logo from "../assets/logo.png";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 function Navbar() {
-    return (
-        <>
-            <div id="nav-bar" className="flex justify-between p-5 px-8 absolute w-full left-0 z-50">
-                <div id="branding">
-                    <img id="logo" src={logo} width={200}></img>
+  const [isOpen, setIsOpen] = useState(false);
 
-                </div>
+  return (
+    <>
+      <div
+        id="nav-bar"
+        className="flex justify-between p-5 px-8 absolute w-full left-0 z-50"
+      >
+        <div id="branding">
+          <img id="logo" src={logo} width={200} alt="Logo" />
+        </div>
 
-                <div id="links" className="flex md:gap-12 md:px-5 mr-2">
-                    <NavLink to="/" className={({ isActive }) => isActive ? 'active' : '' + 'nav-link'}>
-                        Home
-                    </NavLink>
-                    <NavLink to="/about" className={({ isActive }) => isActive ? 'active' : '' + 'nav-link'}>
-                        About
-                    </NavLink>
-                    <NavLink to="/services" className={({ isActive }) => isActive ? 'active' : '' + 'nav-link'}>
-                        Services
-                    </NavLink>
-                    <NavLink to="/contact" className={({ isActive }) => isActive ? 'active' : '' + 'nav-link'}>
-                        Contact
-                    </NavLink>
-                    {/* <a href="/" className="px-2">Home</a>
-                    <a href="/about" className="px-2">About</a>
-                    <a href="/services" className="px-2">Services</a>
-                    <a href="/contact" className="px-2">Contact</a> */}
-                </div>
-            </div>
-        </>
-    );
+        <div
+          id="links"
+          className="flex md:gap-12 md:px-5 mr-2 hidden lg:block"
+        >
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              `${isActive ? "active " : ""}nav-link`
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              `${isActive ? "active " : ""}nav-link`
+            }
+          >
+            About
+          </NavLink>
+          <NavLink
+            to="/services"
+            className={({ isActive }) =>
+              `${isActive ? "active " : ""}nav-link`
+            }
+          >
+            Services
+          </NavLink>
+          <NavLink
+            to="/contact"
+            className={({ isActive }) =>
+              `${isActive ? "active " : ""}nav-link`
+            }
+          >
+            Contact
+          </NavLink>
+        </div>
+
+        {/* Mobile Menu Button */}
+        <div id="menu-bar" className="block lg:hidden">
+          <button
+            className="menu-button"
+            onClick={() => setIsOpen(!isOpen)}
+            aria-label="Toggle menu"
+          >
+            <i
+              className={`fa ${isOpen ? "fa-times" : "fa-bars"}`}
+              aria-hidden="true"
+            ></i>
+          </button>
+        </div>
+      </div>
+
+      {/* Overlay */}
+      {isOpen && (
+        <div
+          className="fixed inset-0 bg-black/50 z-40"
+          onClick={() => setIsOpen(false)}
+        ></div>
+      )}
+
+      {/* Sliding Sidebar */}
+      <div
+        className={`lg:hidden fixed top-0 left-0 w-[65vw] h-[100vh] bg-[linear-gradient(180deg,var(--background),var(--dark-purple))] text-white flex flex-col items-center py-6 pt-30 gap-5 z-40 transform transition-transform duration-300 ease-in-out ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <NavLink
+          to="/"
+          onClick={() => setIsOpen(false)}
+          className={({ isActive }) =>
+            `${isActive ? "active " : ""}nav-link`
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          onClick={() => setIsOpen(false)}
+          className={({ isActive }) =>
+            `${isActive ? "active " : ""}nav-link`
+          }
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/services"
+          onClick={() => setIsOpen(false)}
+          className={({ isActive }) =>
+            `${isActive ? "active " : ""}nav-link`
+          }
+        >
+          Services
+        </NavLink>
+        <NavLink
+          to="/contact"
+          onClick={() => setIsOpen(false)}
+          className={({ isActive }) =>
+            `${isActive ? "active " : ""}nav-link`
+          }
+        >
+          Contact
+        </NavLink>
+      </div>
+    </>
+  );
 }
 
 export default Navbar;
