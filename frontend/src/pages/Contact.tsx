5efd63089aa016bd5a3b0b199bed5202e32251e1
TaigaTi <tarikabirch@gmail.com>
2025-10-05 14:10:52 -0400

    Changed contact page background

--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -1,9 +1,20 @@
 import "./Contact.css";
+import Silk from "../components/Silk";
 
 function Contact() {
     return (
         <>
-            <div id="contact-landing" className="min-h-[100vh] p-20 md:py-40 flex flex-col justify-center items-center text-center">
+            <div id="contact-landing" className="relative min-h-[100vh] p-20 md:py-40 flex flex-col justify-center items-center text-center">
+                  <div className="absolute inset-0 -z-10">
+                    <Silk
+                        speed={8}
+                        scale={1.2}
+                        color="#191239ff"
+                        noiseIntensity={0.6}
+                        rotation={85}
+                    />
+                </div>
+
                 <div id="contact-form" className="flex flex-col justify-center items-center p-10 md:px-30 md:py-20">
                     <h1 className="text-4xl md:text-4xl font-semibold mb-5 text-center">Let's Get in Touch</h1>
                     <form className="flex flex-col justify-start items-start gap-5 w-full ">
