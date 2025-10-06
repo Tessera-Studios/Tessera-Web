76ae9039ff78a7124b9f93c910b60f0437c9ae88
TaigaTi <tarikabirch@gmail.com>
2025-10-06 07:48:51 -0400

    Tweaked spacing on about page

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -8,19 +8,21 @@ import "./About.css";
 function About() {
     return (
         <div className="overflow-x-hidden">
-            <div id="about-landing" className="flex flex-col md:flex-row justify-center items-center min-h-[100vh] px-12 md:p-20" style={{ backgroundImage: `url(${landingImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-                <div className='md:mr-10 px-22 md:p-0'> <img src={emblem} alt="Tessera Emblem" width={300} /></div>
-                <div className="mt-5 md:mt-0 md:w-[35vw] pb-20 md:pb-0">
-                    <h2 className='text-2xl md:text-4xl mb-3 font-semibold'>Every Digital Experience Is A Mosaic</h2>
-                    <p className='md:leading-8'>At Tessera Studios, we believe digital products should feel like art—thoughtfully composed, built with care, and deeply connected to the people who use them.
-                        <br /><br />Our name comes from tesserae, the small, individual pieces of a mosaic—each one essential to the bigger picture.</p>
+            <div id="about-landing" className="flex justify-center items-center min-h-[100vh] px-12 md:p-20" style={{ backgroundImage: `url(${landingImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                <div className="flex flex-col md:flex-row justify-center items-center mb-15">
+                    <div className='md:mr-10 px-22 md:p-0'> <img src={emblem} alt="Tessera Emblem" width={300} /></div>
+                    <div className="mt-5 md:mt-0 md:w-[45vw] pb-20 md:pb-0">
+                        <h2 className='text-2xl md:text-4xl mb-3 font-semibold'>Every Digital Experience Is A Mosaic</h2>
+                        <p className='md:leading-8'>At Tessera Studios, we believe digital products should feel like art—thoughtfully composed, built with care, and deeply connected to the people who use them.
+                            <br /><br />Our name comes from tesserae, the small, individual pieces of a mosaic—each one essential to the bigger picture.</p>
+                    </div>
                 </div>
             </div>
 
             <div id="about-who-we-are" className="flex flex-col md:flex-row justify-center items-center min-h-[100vh] md:min-h-[75vh] px-12 md:p-20 text-white">
                 <div className="lg:w-[36vw] md:p-2 py-10">
                     <h2 className='text-4xl mb-5 font-semibold'>Who We Are</h2>
-                    <p className='md:leading-8 text-sm'>We’re a creative team driven by a shared passion for building meaningful digital experiences.
+                    <p className='md:leading-8 text-sm md:text-base'>We’re a creative team driven by a shared passion for building meaningful digital experiences.
                         <br /><br /> At Tessera Studios, users come first—every product we design and build is rooted in real human needs and thoughtful problem-solving.
                         <br /><br /> We’re committed to crafting immersive, flexible solutions that reflect your vision and deliver real impact.
                     </p>
@@ -45,13 +47,13 @@ function About() {
 
                 <div className="flex flex-col md:flex-row justify-center items-center gap-5">
                     <div className="vision-mission-tile p-10 md:p-20 rounded-lg text-white max-w-4xl z-10 bg-[linear-gradient(to_left,var(--dark-purple),var(--accent))] md:mr-10 text-center min-w-[30vw]">
-                    <h2 className='text-4xl mb-5 font-semibold'>Our Vision</h2>
-                    <p className='text-start'>To be the leading creator of modular, user-centered digital experiences that blend design, technology, and storytelling—empowering businesses and enriching lives, globally.</p>
-                </div>
-                <div className="vision-mission-tile p-10 md:p-20 rounded-lg text-white max-w-4xl z-10 bg-[linear-gradient(to_right,var(--dark-purple),var(--accent))] md:ml-10 text-center min-w-[30vw]">
-                    <h2 className='text-4xl mb-5 font-semibold'>Our Mission</h2>
-                    <p className='text-start'>To craft innovative, scalable digital solutions that are as thoughtful as they are powerful—connecting clients with their users through precision, creativity, and care.</p>
-                </div>
+                        <h2 className='text-4xl mb-5 font-semibold'>Our Vision</h2>
+                        <p className='text-start'>To be the leading creator of modular, user-centered digital experiences that blend design, technology, and storytelling—empowering businesses and enriching lives, globally.</p>
+                    </div>
+                    <div className="vision-mission-tile p-10 md:p-20 rounded-lg text-white max-w-4xl z-10 bg-[linear-gradient(to_right,var(--dark-purple),var(--accent))] md:ml-10 text-center min-w-[30vw]">
+                        <h2 className='text-4xl mb-5 font-semibold'>Our Mission</h2>
+                        <p className='text-start'>To craft innovative, scalable digital solutions that are as thoughtful as they are powerful—connecting clients with their users through precision, creativity, and care.</p>
+                    </div>
                 </div>
             </div>
 
