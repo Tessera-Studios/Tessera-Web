9dd3facd14ebfa075a9410624166e9470a77497b
IsaiahSama <isaiahcarrington46@gmail.com>
2026-01-04 12:53:51 -0400

    Should now redirect to custom success page

--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -31,7 +31,7 @@ function Contact() {
                         name="contact"
                         className="flex flex-col justify-start items-start gap-5 w-full"
                         method="post"
-                        action="/contact-success"
+                        action="/pages/contact-success"
                         data-netlify="true"
                         data-netlify-honeypot="bot-field"
                     >
